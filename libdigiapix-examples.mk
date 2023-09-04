79aa8410c266ad8fd699b8f48936e48b409b6470
Tatiana Leon <Tatiana.Leon@digi.com>
2023-09-04 17:00:31 +0200

    makefile: pm: add missing power management example to the makefile
    Signed-off-by: Tatiana Leon <Tatiana.Leon@digi.com>

--- libdigiapix-examples.mk
+++ libdigiapix-examples.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017 Digi International Inc.
+# Copyright (c) 2017-2023 Digi International Inc.
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
@@ -19,6 +19,7 @@ SUBDIRS := \
 		apix-can-examples \
 		apix-gpio-example \
 		apix-i2c-example \
+		apix-pm-examples \
 		apix-pwm-example \
 		apix-spi-example \
 		apix-watchdog-example
